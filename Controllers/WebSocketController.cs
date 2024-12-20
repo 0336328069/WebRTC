357b3b192070f03fc5c02cf06a86af5b63426590
Hung Ho <hung.ho@mgisolutions.com>
2024-12-20 17:40:42 +0700

    Add project files.

---
+++ Controllers/WebSocketController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WebRTC.Controllers
+{
+    [ApiController]
+    [Route("ws")]
+    public class WebSocketController : ControllerBase
+    {
+        [HttpGet("connect")]
+        public async Task Connect()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await HandleWebSocketAsync(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task HandleWebSocketAsync(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by server", CancellationToken.None);
+                }
+                else
+                {
+                    var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine($"Received: {message}");
+
+                    var responseMessage = Encoding.UTF8.GetBytes($"Echo: {message}");
+                    await webSocket.SendAsync(new ArraySegment<byte>(responseMessage), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+        }
+    }
+}
